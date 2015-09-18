42363f975d8b4c59ac552febfc4c46b813517cb8
Tolya Solovey <seeknowells@gmail.com>
2015-09-19 00:19:09 +0300

    Lol. Ability to follow, like and send messages was blocked by Instagram. API method was failed. #Lehagay#Lets#Start#Scraping

--- app/main.py
+++ app/main.py
@@ -10,52 +10,9 @@ headers = { 'User-Agent' : user_agent,
 likedDict = {}
 
 
-# Auth for Scraping method
-# TODO: Scraping method :D
-# def reqAuth(user, pas):
-#     r = requests.get('https://websta.me/login/', auth=(user, pas))
-#     print(r.status_code)
+#Auth for Scraping method
+#TODO: Scraping method :D
+def reqAuth(user, pas):
+    r = requests.get('https://websta.me/login/', auth=(user, pas))
+    print(r.status_code)
 
-
-client_id = raw_input("Client ID: ").strip()
-client_secret = raw_input("Client Secret: ").strip()
-redirect_uri = raw_input("Redirect URI: ").strip()
-raw_scope = ("").strip()
-scope = raw_scope.split(' ')
-if not scope or scope == [""]:
-    scope = ["basic"]
-api = InstagramAPI(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
-
-redirect_uri = api.get_authorize_login_url(scope = scope)
-print ("Visit this page and authorize access in your browser: "+ redirect_uri)
-code = (str(raw_input("Paste in code in query string after redirect: ").strip()))
-access_token, userdata = api.exchange_code_for_access_token(code)
-print ("access token: " )
-print (access_token)
-
-
-
-def get_relationship(userId):
-    unfollowed=0
-
-    followUrl = "https://api.instagram.com/v1/users/%s/relationship?access_token=%s&client_id=%s" \
-                % (userId, access_token, client_id)
-
-    values = {'access_token' : access_token,
-              'client_id' : client_id}
-    try:
-        data = urllib.urlencode(values)
-        req = urllib2.Request(followUrl,None,headers)
-        response = urllib2.urlopen(req)
-        result = response.read()
-        dataObj = json.loads(result)
-        status = dataObj["data"]
-        incoming = status["incoming_status"]
-        print '%s - %s ' % (userId, incoming)
-        if incoming != "followed_by":
-            return DOES_NOT_FOLLOW
-        else:
-            return FOLLOWS
-    except Exception, e:
-        print e
-    return unfollowed
